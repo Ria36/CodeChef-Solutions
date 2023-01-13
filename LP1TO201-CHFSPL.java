73afc4330a6030642ebaa7581edfeb941f463459
Ria <52408084+Ria36@users.noreply.github.com>
2023-01-13 11:18:49 +0530

    Create LP1TO201-CHFSPL.java

---
+++ LP1TO201-CHFSPL.java
@@ -0,0 +1,30 @@
+/* Chef and Spells */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    
+		    int firstSpell = a+b;
+		    int secondSpell = b+c;
+		    int thirdSpell = a+c;
+		    
+		    int max =  Math.max(Math.max(firstSpell,secondSpell),thirdSpell);
+		    System.out.println(max);
+		}
+	}
+}
