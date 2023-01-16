9df091c8d96bf45f20b38f2bb77353d209209436
Ria <52408084+Ria36@users.noreply.github.com>
2023-01-16 23:16:37 +0530

    Create  LP1TO201- MXEVNSUB.java

---
+++ LP1TO201- MXEVNSUB.java
@@ -0,0 +1,30 @@
+/* Maximum Length Even Subarray */
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int count=0,i;
+		    long sum=0;
+		    for( i=1;i<=n;i++){
+		        sum += i;
+		        count++;
+		    }
+		    if(sum%2 != 0){
+		        count=count-1;
+		    }
+		    System.out.println(count);
+		}
+	}
+}
