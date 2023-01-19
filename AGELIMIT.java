e8f168ef479fbb473252fcb90d017461cc407ff5
Ria <52408084+Ria36@users.noreply.github.com>
2023-01-19 23:35:27 +0530

    Create AGELIMIT.java

---
+++ AGELIMIT.java
@@ -0,0 +1,27 @@
+/* Age Limit */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int a = sc.nextInt();
+		    if(a>=x && a<y)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+	}
+}
