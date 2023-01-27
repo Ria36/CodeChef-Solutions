6a9f03ebc45775142d5b489571431d8fb536cd18
Ria <52408084+Ria36@users.noreply.github.com>
2023-01-27 16:56:37 +0530

    Create Balloon.java

---
+++ Balloon.java
@@ -0,0 +1,30 @@
+/* ICPC Balloons */
+/* https://www.codechef.com/LP1TO205/problems/BALLOON */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Balloon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int a[] = new int[n];
+		    int count = 0;
+		    
+		    for(int i=0;i<n;i++){
+		        a[i] = sc.nextInt();
+		        if(a[i]>=1 && a[i]<=7){
+		            count=i+1;
+		        }
+		    }
+		    System.out.println(count);
+		}
+	}
+}
