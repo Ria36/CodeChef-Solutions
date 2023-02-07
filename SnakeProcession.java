8f8fee21ed697c7cfa4a7e572af732878d8b7a3b
Ria <52408084+Ria36@users.noreply.github.com>
2023-02-07 16:31:24 +0530

    Create SnakeProcession.java

---
+++ SnakeProcession.java
@@ -0,0 +1,27 @@
+/* https://www.codechef.com/problems/SNAKPROC */
+/* Snake Procession */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    String str=sc.next();
+		    str = str.replace(".","");
+		    str = str.replace("HT","");
+		    if(str=="")
+		    System.out.println("Valid");
+		    else
+		    System.out.println("Invalid");
+		}
+	}
+}
