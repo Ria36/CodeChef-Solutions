18d19b399f81b437bfd7db3b5c9f80820d223103
Ria <52408084+Ria36@users.noreply.github.com>
2023-03-01 16:40:19 +0530

    Create Fitness.java

---
+++ Fitness.java
@@ -0,0 +1,22 @@
+/* https://www.codechef.com/problems/FIT */
+/* Fitness */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int result = n*10;
+		    System.out.println(result);
+		}
+	}
+}
