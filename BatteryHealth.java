ff1351d9162ff2b2ef456cd3a44c7aeb3739a0e3
Ria <52408084+Ria36@users.noreply.github.com>
2023-03-06 18:28:13 +0530

    Create BatteryHealth.java

---
+++ BatteryHealth.java
@@ -0,0 +1,24 @@
+/* https://www.codechef.com/problems/BTRYHLTH */
+/* Battery Health */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class BatteryHealth
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    if(x>=80)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+	}
+}
