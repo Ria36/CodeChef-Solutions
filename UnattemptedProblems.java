d4582ff6e708c4c406f692fe9c49f71b39099e10
Ria <52408084+Ria36@users.noreply.github.com>
2023-03-07 12:20:01 +0530

    Create UnattemptedProblems.java

---
+++ UnattemptedProblems.java
@@ -0,0 +1,20 @@
+/* https://www.codechef.com/problems/PRACLIST */
+/* How many unattempted problems */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class UnattemptedProblems
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		System.out.println(x-y);
+	}
+}
+
