dc5f0db855cc9ff8c959a8a80425655a6eb67010
Ria <52408084+Ria36@users.noreply.github.com>
2023-12-10 21:35:00 +0530

    Create ChefAndInstantNoodles.java

---
+++ ChefAndInstantNoodles.java
@@ -0,0 +1,22 @@
+/* https://www.codechef.com/practice/course/java/PJA01/problems */
+/* Chef and Instant Noodles */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ChefAndInstantNoodles
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println(a*b);
+	}
+}
+
