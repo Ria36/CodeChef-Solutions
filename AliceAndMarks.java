e0126975404bd7452bc20ae8c63532e3d5b949a7
Ria <52408084+Ria36@users.noreply.github.com>
2023-12-13 22:06:01 +0530

    Create AliceAndMarks.java

---
+++ AliceAndMarks.java
@@ -0,0 +1,27 @@
+/* https://www.codechef.com/practice/course/java/PJA03/problems/MARKSTW?tab=statement */
+/* Alice and Marks */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class AliceAndMarks
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	Scanner sc=new Scanner (System.in);
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    if(x>=2*y){
+        System.out.println("YES");
+    }
+    else{System.out.println("NO");
+        
+    }
+	}
+}
+
