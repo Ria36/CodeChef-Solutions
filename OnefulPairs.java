9aca5c93be0a1ce7bcd5fcbd14103568864747a1
Ria <52408084+Ria36@users.noreply.github.com>
2023-12-14 18:05:17 +0530

    Create OnefulPairs.java

---
+++ OnefulPairs.java
@@ -0,0 +1,24 @@
+/* https://www.codechef.com/practice/course/java/PJA03/problems/ONEFULPAIRS?tab=statement */
+/* Oneful Pairs */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class OnefulPairs
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    if((a+b+(a*b))==111)
+	    System.out.println("yes");
+	    else
+	    System.out.println("no");
+	}
+}
