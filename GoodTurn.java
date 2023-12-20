b8ad01349da0c9a853da74c5bac9e19a84b34961
Ria <52408084+Ria36@users.noreply.github.com>
2023-12-20 13:55:35 +0530

    Create GoodTurn.java

---
+++ GoodTurn.java
@@ -0,0 +1,31 @@
+/* https://www.codechef.com/practice/course/java/PJA05/problems/GDTURN?tab=statement */
+/* Good Turn */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int n1=sc.nextInt();
+		    int n2=sc.nextInt();
+		    if((n1+n2)>6)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
